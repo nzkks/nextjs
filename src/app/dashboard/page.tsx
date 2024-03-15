10a4efbb41042afe438014ba71f8c525dac0e82a
nzkksdemo <nzkksdemo@gmail.com>
2024-03-15 23:04:18 +1300

    Within a route folder which has page.tsx, if other tsx files kept they are not routable. This is called file colocation

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import LineChart from './line-chart';
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard page
+      <LineChart />
+    </div>
+  );
+};
+
+export default Dashboard;
