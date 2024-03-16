a6b57a9273c0de9221db7a3fe9959549abbee15b
nzkksdemo <nzkksdemo@gmail.com>
2024-03-16 13:12:54 +1300

    Product Details layout: Example usage of the layout can be showing Featured products

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -14,7 +14,13 @@ export default function ProductDetailsLayout({
     <>
       <header>Product Header</header>
       {children}
-      <footer>Product Footer</footer>
+      <div>
+        <div>Featured Products</div>
+        <ul>
+          <li>Featured product 1</li>
+          <li>Featured product 2</li>
+        </ul>
+      </div>
     </>
   );
 }
