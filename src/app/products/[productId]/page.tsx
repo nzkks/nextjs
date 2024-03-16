7b4008d720d690fe9325b70a45f842cd3df4f183
nzkksdemo <nzkksdemo@gmail.com>
2024-03-16 14:44:21 +1300

    Add dynamic route metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,5 +1,13 @@
+import type { Metadata } from 'next';
+
 type ProductDetailsProps = {
   params: { productId: string };
+};
+
+export const generateMetadata = ({ params }: ProductDetailsProps): Metadata => {
+  return {
+    title: `Prooduct ${params.productId}`
+  };
 };
 
 const ProductDetails = ({ params }: ProductDetailsProps) => {
