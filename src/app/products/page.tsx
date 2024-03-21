b56c0f31b752ea7de212d515c40ceb717df524bf
nzkksdemo <nzkksdemo@gmail.com>
2024-03-21 13:34:35 +1300

    Link component with "replace" prop replaces the current history state instead of adding a new URL in the history stack

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,27 @@
 import Link from 'next/link';
 
 const ProductList = () => {
+  const productId = 100;
+
   return (
     <>
       <Link href="/">Home</Link>
       <div>Product list</div>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link href={`products/${productId}`}>Product 1</Link>
+        </li>
+        <li>
+          <Link href={`products/${productId}`}>Product 2</Link>
+        </li>
+        <li>
+          <Link href={`products/${productId}`} replace>
+            Product 3
+          </Link>
+        </li>
+        <li>
+          <Link href={`products/${productId}`}>Product {productId}</Link>
+        </li>
       </ul>
     </>
   );
